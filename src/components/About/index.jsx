69e5597d5ed42f364591511c772b5f213b368208
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-10-27 20:35:53 +0100

    pictures added to about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,5 +1,7 @@
 import "../../assets/styles/components.css";
-
+import Illustration from "../../assets/images/JannieIllustrations.jpg";
+import Portrait from "../../assets/images/JanniePortrait2.jpeg"
+import Fragment from "../../assets/images/JannieFragmentations11.jpeg"
 function About() {
   return (
     <div>
@@ -11,12 +13,26 @@ function About() {
               Create Memories and Stories: Personalized Artwork, Mosaic
               Creations, and Illustrations.
             </h1>
-            <p>Share your vision via email, and
-              I&apos;ll provide a personalised quote. Whether it&apos;s a
-              cherished pet portrait, mosaic-inspired artwork, or illustrations
-              for your storytelling, I&apos;m here to bring your ideas to life.
+            <div className="art-div">
+              <div className="">
+                <h3>Illustrations</h3>
+                <img src={Illustration} className="illustration-image"></img>
+              </div>
+              <div className="">
+              <h3>Portraits</h3>
+                <img src={Portrait} className="portrait-image"></img>
+              </div>
+              <div className="">
+              <h3>Fragmented Mosaic Art</h3>
+                <img src={Fragment} className="fragment-image"></img>
+              </div>
+            </div>
+            <p>
+              Share your vision via email, and I&apos;ll provide a personalised
+              quote. Whether it&apos;s a cherished pet portrait, mosaic-inspired
+              artwork, or illustrations for your storytelling, I&apos;m here to
+              bring your ideas to life.
             </p>
-          
           </div>{" "}
         </div>
       </div>
